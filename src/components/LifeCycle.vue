86e7fed2f81c37abab2cf87e3311c09e70acd492
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-06-11 10:10:09 +0300

    all code

---
+++ src/components/LifeCycle.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <h3>Compnents Life Cycle Hook</h3>
+    <h4>age is {{ age }}</h4>
+    <button @click="age++">inc age</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: "Ahmad",
+      age: "28",
+    };
+  },
+  beforeCreate() {
+    alert("BeforeCreate");
+  },
+  created() {
+    alert("Create");
+  },
+  beforeMount() {
+    alert("BeforeMount");
+  },
+  mounted() {
+    alert("Mounted");
+  },
+  beforeUpdate() {
+    alert("BeforeUpdate");
+  },
+  updated() {
+    alert("Updated");
+  },
+  beforeUnmount() {
+    alert("BeforeUnmount");
+  },
+  unmount() {
+    alert("Unmount");
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld />
+    <LifeCycle />
   </div>
 </template>
 
 <script>
-import HelloWorld from "@/components/HelloWorld.vue";
+import LifeCycle from "@/components/LifeCycle.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    LifeCycle,
   },
 };
 </script>
