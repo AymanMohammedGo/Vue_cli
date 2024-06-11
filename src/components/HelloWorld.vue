beaeba4072f9bc1db5a610998b563867ef316a33
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-06-11 11:26:19 +0300

    all code

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <h2>My Name is {{ name }}</h2>
+    <input type="text" />
+  </div>
 </template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld />
+    <KeepAlive>
+      <HelloWorld :name="name" v-if="!isShow" />
+    </KeepAlive>
   </div>
+  <div><button @click="isShow = !isShow">change state</button></div>
 </template>
 
 <script>
@@ -12,6 +15,12 @@ export default {
   name: "HomeView",
   components: {
     HelloWorld,
+  },
+  data() {
+    return {
+      name: "Ayman",
+      isShow: true,
+    };
   },
 };
 </script>
