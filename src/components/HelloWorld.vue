2c15bc6698563bc2a99a5c4ce1f6fef6d66ac3d6
AymanMohammedGo <ayhammohammed723@gmail.com>
2024-06-18 11:34:47 +0300

    allCode

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,3 +1,31 @@
 <template>
-  <div></div>
+  <div>
+    <H1>Products</H1>
+    <ul v-for="(product, i) in products" :key="i">
+      <li><strong>ID:</strong> {{ product.id }}</li>
+      <li><strong>Title:</strong> {{ product.title }}</li>
+      <li><strong>Category:</strong> {{ product.category }}</li>
+      <hr />
+    </ul>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    async getData() {
+      await fetch("https://dummyjson.com/products")
+        .then((res) => res.json())
+        .then((data) => (this.products = data.products));
+    },
+  },
+  async mounted() {
+    await this.getData();
+  },
+};
+</script>
